870ddc0e054f4cee8a40ab3018e1d06d5b4979ba
Egor Ananich <aevbox@gmail.com>
2019-03-03 19:36:35 +0300

    fix errors

--- src/index.js
+++ src/index.js
@@ -1,20 +1,57 @@
-module.exports = function getZerosCount(number) {
-  let count = 0;
-  for (let i = 5; i < number + 1; i = i + 5) {
-      if (i % 5 === 0) {
-          count++;
-          if ((i / 5) % 5 === 0) {
-              let Nod = i / 5;
-              while (Nod > 1) {
-                  if (Nod % 5 === 0) {
-                      count++;
-                      Nod = (Nod / 5) >> 0;
-                  } else {
-                      Nod = 0;
-                  }
-              }
-          }
-      }
+class ZeroCount {
+  constructor(number, base) {
+    let nok = this.getNOK(base);
+    let maxNok = Math.max.apply(null, nok);
+    let diff = nok.filter(function(item) {
+      return item === maxNok;
+    }).length;
+    let fact = this.factorial(number, maxNok);
+    this.result = (fact / diff) >> 0;
   }
-  return count;
+
+  factorial(n, a) {
+    let count = 0;
+    for (let i = a; i < n + 1; i = i + a) {
+      if (i % a === 0) {
+        count++;
+        if ((i / a) % a === 0) {
+          let N = i / a;
+          let D = a;
+          while (N > 1) {
+            if (N % D === 0) {
+              count++;
+              N = (N / D) >> 0;
+            } else {
+              N = 0;
+            }
+          }
+        }
+      }
+    }
+    return count;
+  }
+
+  getNOK(num) {
+    let j = 0;
+    let i = 2;
+    let a = [];
+    do {
+      if (num % i === 0) {
+        a[j] = i;
+        j++;
+        num = num / i;
+      } else i++;
+    } while (i < num);
+    a[j] = i;
+
+    return a;
+  }
+
+  getResult() {
+    return this.result;
+  }
 }
+
+module.exports = function getZerosCount(number, base) {
+  return new ZeroCount(number, base).getResult();
+};
