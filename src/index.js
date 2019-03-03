d733ce37a32ea55752882127c6ddf58add928d80
Egor Ananich <aevbox@gmail.com>
2019-03-03 19:16:14 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-module.exports = function getZerosCount(number, base) {
-  // your implementation
+module.exports = function getZerosCount(number) {
+  let count = 0;
+  for (let i = 5; i < number + 1; i = i + 5) {
+      if (i % 5 === 0) {
+          count++;
+          if ((i / 5) % 5 === 0) {
+              let Nod = i / 5;
+              while (Nod > 1) {
+                  if (Nod % 5 === 0) {
+                      count++;
+                      Nod = (Nod / 5) >> 0;
+                  } else {
+                      Nod = 0;
+                  }
+              }
+          }
+      }
+  }
+  return count;
 }
